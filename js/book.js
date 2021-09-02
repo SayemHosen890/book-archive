fdd8bea507b6f68dd963a9a769359f13d35990c2
SayemHosen890 <sayemhosen902@gmail.com>
2021-09-02 16:56:41 +0600

    fourth commit

--- js/book.js
+++ js/book.js
@@ -1,14 +1,25 @@
+document.getElementById('error-message').style.display = 'none';
 //load data from api
 const searchBook = () => {
     const searchInput = document.getElementById('search-input');
     const searchField = searchInput.value;
+    //clear data
     searchInput.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchField}`;
-    console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayBook(data.docs))
+    if (searchField === '') {
+        // console.log('try again');
+        // displayResult.textContent = '';
+        document.getElementById('error-message').style.display = 'block';
+    }
+    else {
+        document.getElementById('error-message').style.display = 'none';
+        const url = `https://openlibrary.org/search.json?q=${searchField}`;
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayBook(data.docs))
+    }
 }
+
 //display data on the website
 const displayBook = books => {
     console.log(books[0]);
@@ -19,13 +30,13 @@ const displayBook = books => {
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card container">
-                <div class="card-body w-75 mx-auto">
-                    <img src="https://covers.openlibrary.org/b/id/554106-M.jpg"class="card-img-top w-50" alt="...">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h5 class="card-text">${book.author_name}</h5>
-                    <h5 class="card-text">${book.first_publish_year}<h5>
-                    <h5 class="card-text">${book.publisher}</h5>
+        <div class="card">
+                <div class="card-body w-75 h-75 mx-auto">
+                    <img src="https://covers.openlibrary.org/b/id/554106-M.jpg"class="card-img-top" alt="...">
+                    <h5 class="card-title">Book Name : ${book.title}</h5>
+                    <h5 class="card-text">Author Name : ${book.author_name[0]}</h5>
+                    <h5 class="card-text">Publisher : ${book.publisher[0]}</h5>
+                    <h5 class="card-text">First Publish Date : ${book.first_publish_year}<h5>
                 </div>
         </div>
         `;
